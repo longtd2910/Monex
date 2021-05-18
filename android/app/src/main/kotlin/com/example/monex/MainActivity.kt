90ac010184b5d5407a8e2a322b86b62aa1a7af56
longtd2910 <longtd2910@gmail.com>
2021-05-18 23:25:18 +0700

    update app name, package name for android

--- android/app/src/main/kotlin/com/example/monex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.monex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/longtd/monex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.longtd.monex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
